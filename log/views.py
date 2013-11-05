806a7cdc9fce0d7ccf07738251c17920815ec6fe
Patrick Wilson <patrick.wilson.14@gmail.com>
2013-11-04 16:15:26 -0800

    Added signup functionality, no error handling yet though. Also fixed typo in brand section of toolbar

--- log/views.py
+++ log/views.py
@@ -1,4 +1,3 @@
-# Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 from django import forms
@@ -7,6 +6,7 @@ from django.template import RequestContext
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.models import User
 
 def login(request):
     username = password = ''
@@ -29,4 +29,23 @@ def logout(request):
     auth_logout(request)
     return HttpResponseRedirect('/')
 
-# def signup(request):
+# TODO - Error handling mechanisms
+def signup(request):
+    username = password = email = fname = lname = ""
+    error = False
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+
+        user = User.objects.create_user(username, email, password)
+        user.first_name = fname
+        user.last_name = lname
+        user.save()
+
+        #Now, log in user and redirect to home
+        user = authenticate(username = username, password = password)
+        auth_login(request, user)
+        return HttpResponseRedirect('/')
